2c7d5ad6fb040fea2eb9427088a620e90428f5cf
Nihar Thakkar <nihartar@gmail.com>
2017-12-21 13:04:20 +0530

    Version 1.0.

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,6 @@
 public class Constants
 {
-    public static final String STAGE_TITLE = "ARK-Cactus (Demo)";
+    public static final String STAGE_TITLE = "ARK-Cactus";
     public static final int STAGE_HEIGHT = 768;
     public static final int STAGE_WIDTH = 1366;
 }
